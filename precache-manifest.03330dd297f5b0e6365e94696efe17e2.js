6fbfef72f989c8fd751186bd9280800c5c8b3c92
KU <olegdkma@gmail.com>
2020-07-23 13:01:10 +0300

    Updates

---
+++ precache-manifest.03330dd297f5b0e6365e94696efe17e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b7e4d80315cfd07847414a96ca5556",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "96f9d6f653a765fbff9f",
+    "url": "/react-game/static/css/main.1e860f4a.chunk.css"
+  },
+  {
+    "revision": "9109e7d63f4da60f55eb",
+    "url": "/react-game/static/js/2.f9d2a18d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-game/static/js/2.f9d2a18d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f9d6f653a765fbff9f",
+    "url": "/react-game/static/js/main.d8101920.chunk.js"
+  },
+  {
+    "revision": "d16c3e23a797ac7e7ada",
+    "url": "/react-game/static/js/runtime-main.28aa4291.js"
+  }
+]);
